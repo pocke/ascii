f503c434a3da7cb6b6439514c09986c63c15af3e
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-07-20 13:00:00 +0900

    Reverse option

--- ascii.go
+++ ascii.go
@@ -1,25 +1,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
 )
 
+var flag_reverse bool
+
 func main() {
-	for _, v := range os.Args[1:] {
-		s, err := ASCII(v)
-		if err != nil {
-			panic(err)
+	f := flag.NewFlagSet("ascii", flag.ExitOnError)
+	f.BoolVar(&flag_reverse, "reverse", false, "String to ascii code")
+	f.Parse(os.Args[1:])
+
+	for _, v := range f.Args() {
+		if !flag_reverse {
+			s, err := ASCII(v)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(s)
+		} else {
+			for _, ch := range []byte(v) {
+				s, err := ASCIIR(string(ch))
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println(s)
+			}
 		}
-		fmt.Println(s)
 	}
 }
 
+// "65" => "A"
 func ASCII(code string) (string, error) {
 	i, err := strconv.Atoi(code)
 	if err != nil {
 		return "", err
 	}
 	return string(i), nil
+}
+
+// "A" => "65"
+func ASCIIR(str string) (string, error) {
+	if len(str) != 1 {
+		return "", fmt.Errorf("")
+	}
+	return fmt.Sprintf("%d", int([]byte(str)[0])), nil
 }
